2ce946ca29bf9399b5ec0e20ca624fcb64a44b27
Sashank Singh <sashanksingh1983@gmail.com>
2024-07-01 15:53:10 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://sashanksingh1983:lvSHnA79mReqOkDQ@cluster0.s3mp5jt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true,dbName: "quotes" })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Quote schema and model
+const quoteSchema = new mongoose.Schema({
+  text: String,
+  author: String
+});
+
+const Quote = mongoose.model('Quote', quoteSchema);
+
+// Sample quotes array
+const quotes = [];
+
+// Routes
+app.get('/api/quotes/random', async (req, res) => {
+  const count = await Quote.countDocuments();
+  const random = Math.floor(Math.random() * count);
+  const quote = await Quote.findOne().skip(random);
+  res.json(quote);
+});
+
+app.get('/api/quotes/search', async (req, res) => {
+  const author = req.query.author;
+  const quotes = await Quote.find({ author: new RegExp(`^${author}$`, 'i') });
+  res.json(quotes);
+});
+
+app.get('/api/quotes', async (req, res) => {
+  try {
+    const quotes = await Quote.find();
+    res.json(quotes);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
