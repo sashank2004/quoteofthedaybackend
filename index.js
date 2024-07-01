1db714adced6d191b62fa8982f6e5e3145e16bcc
sashank2004 <134407020+sashank2004@users.noreply.github.com>
2024-07-01 17:38:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://sashanksingh1983:lvSHnA79mReqOkDQ@cluster0.s3mp5jt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true,dbName: "quotes" })
+mongoose.connect('mongodb+srv://sashanksingh1983:lvSHnA79mReqOkDQ@cluster0.s3mp5jt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {dbName: "quotes" })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
